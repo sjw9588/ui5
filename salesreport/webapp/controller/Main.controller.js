3bbc119de38d7207c64ee2b99ec9efab59dcb4cb
sjw9588 <sjw9588@gmail.com>
2025-03-14 16:00:12 +0900

    salesreport

--- salesreport/webapp/controller/Main.controller.js
+++ salesreport/webapp/controller/Main.controller.js
@@ -1,12 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], (Controller,
+	JSONModel) => {
     "use strict";
 
     return Controller.extend("sync.c10.salesreport.controller.Main", {
         onInit() {
+            var oModel = new JSONModel({
+                Unit: "EA",
+            });
+            // view에  "view"라는 이름으로 모델 정의
+            // view>/Unit
+            this.getView().setModel(oModel, "view");
         },
-        onPressItemClick(oEvent){
+        // item 클릭 되었을 때
+        // 1. detail view에 정보 binding
+        // 2. 정보 fragment 띄우기
+        async onPressItemClick(oEvent){
             // 클릭한 행 정보
             var oItem = oEvent.getSource();
 
@@ -28,11 +39,25 @@ sap.ui.define([
                 model : "sales",
             });
 
-            // // fragment load (비동기)
-            // this.oDialog ??= await this.loadFragment({
-            //     name: "sync.c10.salesreport.view.Detail"
-            // });
-            // this.oDialog.open(); // fragment open
+            // fragment load (비동기)
+            this.oDialog ??= await this.loadFragment({
+                name: "sync.c10.salesreport.view.Detail"
+            });
+
+            // load 해온 fragment에 bindElement
+            this.oDialog.bindElement({
+                path:sPath,
+                model: "sales"
+            })
+            this.oDialog.open(); // fragment open
+        },
+
+        // dialog 닫기 구현
+        onCloseDialog(){
+            var oDialog = this.byId("idDialog");
+            if( oDialog ) {
+                oDialog.close();
+            }
         }
     });
 });
